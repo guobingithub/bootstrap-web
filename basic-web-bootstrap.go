e89ac23723958ce17f20a5f2f4d521d652060430
guobin <596194783@qq.com>
2018-11-16 19:05:22 +0800

    first commit

---
+++ basic-web-bootstrap.go
@@ -0,0 +1,42 @@
+/*
+ * @Author: bingo
+ * @Date: 2018-11-16 10:55:02
+ */
+package main
+
+import "fmt"
+import "net/http"
+import "html/template"
+
+func Hello(response http.ResponseWriter, request *http.Request) {
+	type person struct {
+		Id      int
+		Name    string
+		Country string
+	}
+
+	liumiaocn := person{Id: 1001, Name: "liumiaocn", Country: "China"}
+
+	tmpl, err := template.ParseFiles("./views/user.tpl")
+	if err != nil {
+		fmt.Println("Error happened..")
+	}
+	tmpl.Execute(response, liumiaocn)
+}
+
+func First_bts(response http.ResponseWriter, request *http.Request) {
+	tmpl, err := template.ParseFiles("./views/first_bts.tpl")
+	if err != nil {
+		fmt.Println("Error happened..")
+	}
+	tmpl.Execute(response, nil)
+}
+
+func main() {
+	http.Handle("/css/", http.StripPrefix("/css/", http.FileServer(http.Dir("./css"))))
+
+	http.HandleFunc("/", Hello)
+	http.HandleFunc("/first",First_bts)
+
+	http.ListenAndServe(":8080", nil)
+}
