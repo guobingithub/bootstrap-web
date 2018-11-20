edd742064867df04f885eda6563353f4d971fb19
guobin <596194783@qq.com>
2018-11-20 17:59:50 +0800

    gin-example

---
+++ gin-example/main.go
@@ -0,0 +1,23 @@
+/*
+ * @Author: bingo
+ * @Date: 2018-11-16 10:55:02
+ */
+package main
+
+import (
+	"net/http"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	router := gin.Default()
+	//router.LoadHTMLGlob("templates/*")
+	router.LoadHTMLGlob("C:/My/gopath/src/bootstrap-web/templates/*")
+	//router.LoadHTMLFiles("templates/template1.html", "templates/template2.html")
+	router.GET("/index", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.tmpl", gin.H{
+			"title": "Main website---主页",
+		})
+	})
+	router.Run(":8070")
+}
